8101e49601f61451351cc7ea583151e624c3770e
michael <connellmp@gmail.com>
2021-03-16 16:35:12 -0700

    move sol files to smart contracts folder

--- BlandContract.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.7.4;
-
-contract BlandContract {
-    string public blandString = 'hello world';
-}
--- Debugging.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.7.4;
-
-contract DebuggerExample {
-    uint public myUint;
-
-    function setMyUint(uint _myuint) public {
-        myUint = _myuint;
-    }
-}
