0f1cde6e828771ff78a2ce60f53bf19416012a16
michael <connellmp@gmail.com>
2021-03-15 17:25:50 -0700

    make error message more specific

--- Owned.sol
+++ Owned.sol
@@ -9,7 +9,7 @@ contract Owned {
     modifier onlyOwner() {
         // need to add the modifier between public and curly brackets for the
         // functions that will use it
-        require(msg.sender == owner, "oh no you don't!");
+        require(msg.sender == owner, "only owner can call this function!");
         _;
         // the function body during compilation is being copied to the modifier
         // ond then back to the function body
