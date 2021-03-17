615fe6a3ebe0f2bc9147a23707aa6c69e44a8fd6
michael <connellmp@gmail.com>
2021-03-16 17:11:32 -0700

    create libraries file

---
+++ smart_contracts/Libraries.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.7.4;
+
+contract LibrariesExample {
+
+    mapping(address => uint) public tokenBalance;
+
+    constructor() {
+        tokenBalance[msg.sender] = 1;
+    }
+
+    function sendToken(address _to, uint _amount) public returns(bool) {
+        tokenBalance[msg.sender] -= _amount;
+        tokenBalance[_to] += _amount;
+
+        return true;
+    }
+}
