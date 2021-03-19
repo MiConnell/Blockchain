e1737e3819a3953e042f513ddb6d67d685281b06
michael <connellmp@gmail.com>
2021-03-18 17:45:42 -0700

    update security to use openzeppelin

--- wallet/SimpleWallet.sol
+++ wallet/SimpleWallet.sol
@@ -1,21 +1,9 @@
 pragma solidity ^0.7.4;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol"
 
-contract SimpleWallet {
+contract SimpleWallet is Ownable {
 
-    address public owner;
-
-    constructor() {
-        owner = msg.sender;
-    }
-
-    // verify that owner of the funds is the message sender
-    modifier onlyOwner() {
-        require(owner == msg.sender, "you are not allowed");
-        _;
-    }
-
-    // use modifier to ensure only the owner can interact with the funds
     function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
         _to.transfer(_amount);
     }
