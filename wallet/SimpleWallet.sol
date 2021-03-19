9605445e9ce9334ef87f43e1142476a0e23ef6ab
michael <connellmp@gmail.com>
2021-03-18 17:34:24 -0700

    create wallet dir

---
+++ wallet/SimpleWallet.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.7.4;
+
+
+contract SimpleWallet {
+
+    function withdrawMoney(address payable _to, uint amount) public {
+        _to.transfer(_amount);
+    }
+
+    function() external payable {
+        
+    }
+
+
+}
