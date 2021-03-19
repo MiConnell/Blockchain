73cf0697b6ba7da05e342aac11f85a4a184c6157
michael <connellmp@gmail.com>
2021-03-18 17:38:45 -0700

    add security functionality

--- wallet/SimpleWallet.sol
+++ wallet/SimpleWallet.sol
@@ -3,12 +3,25 @@ pragma solidity ^0.7.4;
 
 contract SimpleWallet {
 
-    function withdrawMoney(address payable _to, uint amount) public {
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    // verify that owner of the funds is the message sender
+    modifier onlyOwner() {
+        require(owner == msg.sender, "you are not allowed");
+        _;
+    }
+
+    // use modifier to ensure only the owner can interact with the funds
+    function withdrawMoney(address payable _to, uint amount) public onlyOwner {
         _to.transfer(_amount);
     }
 
     function() external payable {
-        
+
     }
 
 
