4e49e8b1fbe707d218b758736b24358318eb07d0
michael <connellmp@gmail.com>
2021-03-20 16:56:16 -0700

    move allowance contract to separate file

--- wallet/SimpleWallet.sol
+++ wallet/SimpleWallet.sol
@@ -1,53 +1,25 @@
 pragma solidity ^0.7.4;
-
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol"
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/utils/math/SafeMath.sol";
-
-contract Allowance is Ownable {
-
-    using SafeMath for uint
-
-    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
-
-    mapping(address => uint) public allowance;
-
-    function addAllowance(address _who, uint _amount) public onlyOwner {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
-        allowance[_who] = _amount;
-    }
-
-    modifier ownerOrAllowed(uint _amount) {
-        require(isOwner() || allowance[msg.sender] >= _amount, "you are not allowed")
-    }
-
-    function reduceAllowance(address _who, uint _amount) internal {
-        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who].sub(_amount));
-        require(_amount <= address(this).balance, "not enough funds")
-        allowance[_who] = allowance[_who].sub(_amount);
-    }
-
-}
+import "./Allowance.sol";
 
 contract SimpleWallet is Allowance {
 
-    event MoneySent(address indexed _beneficiary, uint _amount)
-    event MoneyReceived(address indexed _from, uint _amount)
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
 
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         if(!isOwner()) {
-            reduceAllowance(msg.sender, _amount)
+            reduceAllowance(msg.sender, _amount);
         }
-        emit MoneySent(_to, _amount)
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
     function renounceOwnership() public onlyOwner {
-        revert("Can't renouce ownership")
+        revert("Can't renouce ownership");
     }
 
     fallback() external payable {
-        emit MoneyReceived(msg.sender, msg.value)
-
+        emit MoneyReceived(msg.sender, msg.value);
     }
 
 }
