1f8efa539edd865e02953ec5c4e692f7d53e47ba
michael <connellmp@gmail.com>
2021-03-20 16:44:45 -0700

    add events for SimpleWallet contract

--- wallet/SimpleWallet.sol
+++ wallet/SimpleWallet.sol
@@ -27,14 +27,19 @@ contract Allowance is Ownable {
 
 contract SimpleWallet is Allowance {
 
+    event MoneySent(address indexed _beneficiary, uint _amount)
+    event MoneyReceived(address indexed _from, uint _amount)
+
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
         if(!isOwner()) {
             reduceAllowance(msg.sender, _amount)
         }
+        emit MoneySent(_to, _amount)
         _to.transfer(_amount);
     }
 
     fallback() external payable {
+        emit MoneyReceived(msg.sender, msg.value)
 
     }
 
