c304ee3979b77dc5472b661a1751878743a68bb5
michael <connellmp@gmail.com>
2021-03-20 16:42:46 -0700

    add events for allowance contract

--- wallet/SimpleWallet.sol
+++ wallet/SimpleWallet.sol
@@ -4,9 +4,12 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/contracts/access/
 
 contract Allowance is Ownable {
 
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom, uint _oldAmount, uint _newAmount);
+
     mapping(address => uint) public allowance;
 
     function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
     }
 
@@ -15,6 +18,7 @@ contract Allowance is Ownable {
     }
 
     function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
         require(_amount <= address(this).balance, "not enough funds")
         allowance[_who] -= _amount;
     }
@@ -33,6 +37,5 @@ contract SimpleWallet is Allowance {
     fallback() external payable {
 
     }
-
 
 }
