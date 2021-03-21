59efe191d7b594e43995d17dc37524518a6cf157
michael <connellmp@gmail.com>
2021-03-20 17:31:56 -0700

    send ether back to original address

--- web3/web3_example.js
+++ web3/web3_example.js
@@ -6,11 +6,10 @@ web3.eth.getBalance("0x677F8051E787721507C8E711a09b7D776589aE56").then(
     function (result) { console.log(web3.utils.fromWei(result, "ether")); }
 );
 
-
 web3.eth.sendTransaction(
     {
-        from: "0x677F8051E787721507C8E711a09b7D776589aE56",
-        to: "0x288550B0cB70342969DC391a05401eD8c0e14509",
+        from: "0x288550B0cB70342969DC391a05401eD8c0e14509",
+        to: "0x677F8051E787721507C8E711a09b7D776589aE56",
         value: web3.utils.toWei("1", "ether")
     }
 );
