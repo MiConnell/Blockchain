2a20dcc80896994cd45c60a9b21db2f3abf9e171
michael <connellmp@gmail.com>
2021-03-23 16:37:00 -0700

    triple equals

--- supply_chain/_truffle/client/src/App.js
+++ supply_chain/_truffle/client/src/App.js
@@ -43,7 +43,7 @@ class App extends Component {
 
     handleInputChange = (event) => {
         const target = event.target;
-        const value = target.type == "checkbox" ? target.checked : target.value;
+        const value = target.type === "checkbox" ? target.checked : target.value;
         const name = target.name;
         this.setState({
             [name]: value
