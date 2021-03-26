dd3867f1a1d554a9eea75650fbd99263240f83c2
michael <connellmp@gmail.com>
2021-03-25 17:18:35 -0700

    create unit tests

---
+++ supply_chain/_truffle/test/testitemmanage.js
@@ -0,0 +1,22 @@
+const { assert } = require("node:console");
+
+const ItemManager = artifacts.require("./ItemManager.sol");
+
+contract("Item Manager", (accounts) => {
+  it("should be able to add an item", async function () {
+    const itemManagerInstance = await ItemManager.deployed();
+    const itemName = "testItem";
+    const itemPrice = 500;
+
+    const result = await itemManagerInstance.createItem(itemName, itemPrice, {
+      from: accounts[0],
+    });
+    assert.equal(
+      result.logs[0].args._itemIndex,
+      0,
+      "This is not the first item"
+    );
+    const item = await itemManagerInstance.items(0);
+    assert.equal(item._identifier, itemName, "The identifier was different");
+  });
+});
