4f92d808a3508ead309d4d15480e00b6cb43d7cd
michael <connellmp@gmail.com>
2021-05-13 18:24:52 -0700

    update and add tests

--- tokenization/contracts/MyToken.sol
+++ tokenization/contracts/MyToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract MyToken is ERC721 {
-    constructor() ERC721("Skoochi Token", "SKOOCH") {
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Skoochi", "SKOOCH") {
+        _mint(msg.sender, initialSupply);
     }
 }
